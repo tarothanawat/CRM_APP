9d558b42ae526d3d7f5a479296f12bc65d89827d
Thanawat Tantijaroensin <taroofsatoeimo@gmail.com>
2024-09-29 21:06:12 +0700

    first commit

--- website/models.py
+++ website/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    student_id = models.CharField(max_length=10, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
